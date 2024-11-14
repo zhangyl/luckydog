95479578814204861d5efbeff3ea8f148770a967
Zhangyuliang <zhangyl@maycur.com>
2024-11-14 09:11:26 +0800

    init

---
+++ app/src/main/java/com/fast/snail/lukydog/App.java
@@ -0,0 +1,11 @@
+package com.fast.snail.lukydog;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
