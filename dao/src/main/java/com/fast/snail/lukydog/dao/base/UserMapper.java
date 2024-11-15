e502cc16eeafe71d7c2dd6f027d689a2f70ac36b
Zhangyuliang <zhangyl@maycur.com>
2024-11-15 21:18:48 +0800

    init

---
+++ dao/src/main/java/com/fast/snail/lukydog/dao/base/UserMapper.java
@@ -0,0 +1,16 @@
+package com.fast.snail.lukydog.dao.base;
+
+
+import com.fast.snail.lukydog.dao.base.object.UserDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+
+    UserDO selectByPrimaryKey(@Param("userCode") String userCode);
+
+    int insert(@Param("userDO") UserDO userDO);
+
+    int updateUserByPrimaryKey(@Param("userDO") UserDO userDO);
+}
---
+++ dao/src/main/java/com/fast/snail/lukydog/dao/base/object/UserDO.java
@@ -0,0 +1,79 @@
+package com.fast.snail.lukydog.dao.base.object;
+
+import java.util.Date;
+import java.util.Map;
+
+public class UserDO {
+    private String userCode;
+    private String entCode;
+    private String userName;
+    private String phoneNumber;
+    private String userStatus;
+    private Date createTime;
+    private Date updateTime;
+    private Map<String, Object> extraData;
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEntCode() {
+        return entCode;
+    }
+
+    public void setEntCode(String entCode) {
+        this.entCode = entCode;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public Map<String, Object> getExtraData() {
+        return extraData;
+    }
+
+    public void setExtraData(Map<String, Object> extraData) {
+        this.extraData = extraData;
+    }
+}
